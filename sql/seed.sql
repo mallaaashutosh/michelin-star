6cfd3303afe0fc0d8e64f60c13d39b1aa8db9fe0
Archana Bhattarai Sharma <archanabhattarai42@gmail.com>
2026-05-03 15:58:23 +0545

    gfghj

--- sql/seed.sql
+++ sql/seed.sql
@@ -20,65 +20,65 @@ ALTER TABLE payment AUTO_INCREMENT = 1;
 ALTER TABLE orders AUTO_INCREMENT = 1;
 ALTER TABLE booking AUTO_INCREMENT = 1;
 
-INSERT INTO admin (name) VALUES
-('Archana Bhattarai Sharma'),
-('Anisha Gurung'),
-('Preeti Kumari Dhwmala'),
-('Aasutosh Malla'),
-('Unika Aadikari');
+INSERT INTO admin (name, email, password) VALUES
+('Archana Bhattarai Sharma', 'archanabhattarai@gmail.com', 'archana123'),
+('Anisha Gurung' 'anishagurung@gmail.com', 'anisha123'),
+('Preeti Kumari Dhwmala', 'preetikumaridhamala@gmail.com', 'preeti123'),
+('Aasutosh Malla', 'aasutoshMalla@gmail.com', 'aasutosh123'),
+('Unika Aadikari', 'unikaadhikari@gmail.com', 'unika123');
 
-INSERT INTO staff (name, role, phone_number) VALUES
-('Bibisha','Manager','9845643889'),
-('Pooja', 'Waiter','9845987657'),
-('Kristina','Waiter','9834876543'),
-('Rakshya','Cook','9853487264'),
-('Sristy','Waiter','9836091876');
+INSERT INTO staff (name, role, phone_number, email, password, status) VALUES
+('Bibisha','Manager','9845643889', 'bibisha@gmail.com', 'bibisha123', 'active'),
+('Pooja', 'Waiter','9845987657', 'pooja@gmail.com', 'pooja123', 'active'),
+('Kristina','Waiter','9834876543', 'kristina@gmail.com', 'kristina123', 'active'),
+('Rakshya','Cook','9853487264', 'rakshya@gmail.com', 'rakshya123', 'active'),
+('Sristy','Waiter','9836091876', 'sristy@gmail.com', 'sristy123', 'active');
 
-INSERT INTO customer (name, phone_number, email) VALUES
-('Hari KC','9845987657', 'hari@gmail.com'),
-('Nirajan aadhikari','9845643889', 'nirajan@gmail.com'),
-('Jwola Shrestha','9834876543', 'jwola@gmail.com'),
-('Anita Poudel','9836091876', 'anita@gmail.com'),
-('Suikriti Aryal','9836091876','Suikriti@gmail.com');
+INSERT INTO customer (name, phone_number, email, password, status) VALUES
+('Hari KC','9845987657', 'hari@gmail.com', 'hari123', 'pending'),
+('Nirajan aadhikari','9845643889', 'nirajan@gmail.com', 'nirajan@gmail.com', 'niraijan123', 'pending'),
+('Jwola Shrestha','9834876543', 'jwola@gmail.com', 'jwola@gmail.com', 'jwola123', 'pending'),
+('Anita Poudel','9836091876', 'anita@gmail.com', 'anita@gmail.com', 'anita123', 'pending'),
+('Suikriti Aryal','9836091876','Suikriti@gmail.com', 'suikriti@gmail.com', 'suikriti123', 'pending');
 
-INSERT INTO menu (category, price) VALUES
-('SteamMomo',180),
-('FriedMomo - Fried', 200),
-('Dal Bhat Tarkari',250),
-('Chicken Chowmein',220),
-('Veg Fried Rice',190);
+INSERT INTO menu (name, category, price, availability) VALUES
+('SteamMomo', 'Japanese', 180.00, 'available'),
+('FriedMomo - Fried', 'Indian', 200.00, 'available'),
+('Dal Bhat Tarkari', 'Nepali',250.00, 'available'),
+('Chicken Chowmein', 'Italian',220.00, 'available'),
+('Veg Fried Rice', 'Turkish', 190.00, 'available');
 
-INSERT INTO orders (customer_id, menu_id, dates) VALUES
-(1, 1,  '2026-04-10'),
-(2, 5,  '2026-04-10'),
-(3, 3,  '2026-04-11'),
-(4, 3,  '2026-04-11'),
-(5, 2,  '2026-04-12');
+INSERT INTO orders (customer_id, menu_id, dates, quantity, total_price, status) VALUES
+(1, 1,  '2026-04-10', 1, 200.00, 'pending'),
+(2, 5,  '2026-04-10', 1, 350.00, 'pending'),
+(3, 3,  '2026-04-11', 1, 260.00, 'pending'),
+(4, 3,  '2026-04-11', 1, 550.00, 'pending'),
+(5, 2,  '2026-04-12', 1, 180.00, 'pending');
 
 INSERT INTO receipt (order_id, total_amount) VALUES
-(1,  180),
-(2,  190),
-(3,  250),
-(4,  450),
-(5,  280);
+(1,  180.00),
+(2,  190.00),
+(3,  250.00),
+(4,  450.00),
+(5,  280.00);
 
-INSERT INTO tables (capacity, location) VALUES
-(2,  'Roof'),
-(4,  'Outdoor'),
-(4,  'Roof'),
-(6,  'Indoor'),
-(2,  'Outdoor');
+INSERT INTO tables (capacity, location, table_number, status) VALUES
+(2,  'Roof', 6, 'available'),
+(4,  'Outdoor', 3, 'available'),
+(4,  'Roof', 1, 'available'),
+(6,  'Indoor', 4, 'available'),
+(2,  'Outdoor', 2, 'available');
 
-INSERT INTO payment (order_id, amount, dates) VALUES
-(1,  180, '2026-04-10'),
-(2,  190, '2026-04-10'),
-(3,  250, '2026-04-11'),
-(4,  450, '2026-04-11'),
-(5,  280, '2026-04-12');
+INSERT INTO payment (order_id, amount, dates, method, status) VALUES
+(1,  180.00, '2026-04-10', 'cash', 'unpaid'),
+(2,  190.00, '2026-04-10', 'cash', 'unpaid'),
+(3,  250.00, '2026-04-11', 'cash', 'unpaid'),
+(4,  450.00, '2026-04-11', 'cash', 'unpaid'),
+(5,  280.00, '2026-04-12', 'cash', 'unpaid');
 
-INSERT INTO booking (customer_id, table_id, dates) VALUES
-(1, 1, '2026-04-15'),
-(2, 2, '2026-04-16'),
-(3, 3, '2026-04-17'),
-(4, 4, '2026-04-18'),
-(5, 5, '2026-04-19');
+INSERT INTO booking (customer_id, table_id, dates, times, status) VALUES
+(1, 1, '2026-04-15', '12:30', 'pending'),
+(2, 2, '2026-04-16', '7:00', 'pending'),
+(3, 3, '2026-04-17', '12:00', 'pending'),
+(4, 4, '2026-04-18', '1:50', 'pending'),
+(5, 5, '2026-04-19', '5:30', 'pending');
